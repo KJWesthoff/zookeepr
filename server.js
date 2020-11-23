e8f7a49a046d3371246df2b3bf0941d662db5b1a
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2020-11-23 13:41:49 -0800

    choose params

--- server.js
+++ server.js
@@ -45,6 +45,12 @@ function filterByQuery(query, animalsArray) {
 
 
 
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
+
 app.get('/api/animals', (req,res) =>{
     let results = animals;
     if (req.query) {
@@ -52,6 +58,16 @@ app.get('/api/animals', (req,res) =>{
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if(result){
+    res.json(result);
+  } else {
+    res.sendStatus(404);
+  } 
+});
+
 
 
 app.listen(PORT, () => {
