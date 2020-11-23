58c0fe6d1b755e01c00a0b0d3ee472feedf1ada9
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2020-11-23 13:27:04 -0800

    port change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { animals } = require('./data/animals');
 
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -54,7 +54,7 @@ app.get('/api/animals', (req,res) =>{
 });
 
 
-app.listen(3001, () => {
-    console.log("API erver now on port 3001")
+app.listen(PORT, () => {
+    console.log(`API erver now on port ${PORT}`)
 });
 
