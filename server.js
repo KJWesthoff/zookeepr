807f0f3c591e5911658255edc49721aecffa5acf
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2020-11-26 09:34:47 -0800

    frontend wired

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-
+app.use(express.static('public'));
 
 
 function filterByQuery(query, animalsArray) {
@@ -97,7 +97,17 @@ function validateAnimal(animal) {
   return true;
 };
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
 
 app.get('/api/animals', (req,res) =>{
     let results = animals;
